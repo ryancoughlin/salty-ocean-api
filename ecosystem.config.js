98a6a4af55ca0ba2f8edc55138b8ca7e32b8d81b
Ryan Coughlin <ryan@ryancoughlin.com>
2025-01-07 20:48:49 -0500

    Enhance ecosystem configuration for salty-server
    - Updated ecosystem.config.js to include the Node.js interpreter path for better compatibility.
    - Improved formatting and readability of the configuration file for easier maintenance.
    
    These changes aim to streamline the server setup process and ensure consistent execution in production environments.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,10 @@
-module.exports = { apps: [{ name: "salty-server", script: "app.js", instances: 1, autorestart: true, watch: false, max_memory_restart: "1G", env: { NODE_ENV: "production", PORT: 5010 } }] }
+module.exports = {
+    apps: [{ 
+        name: "salty-server",
+        script: "app.js", 
+        interpreter: '/usr/bin/node', 
+        instances: 1,
+        autorestart: true,
+        env: { NODE_ENV: "production", PORT: 5010 } 
+    }]
+}

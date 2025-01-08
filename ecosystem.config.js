6c3f641dd59c3b7cf1d457eccc0ead30a66a1aba
Ryan Coughlin <ryan@ryancoughlin.com>
2025-01-07 20:50:56 -0500

    Update Node.js interpreter path in ecosystem configuration for salty-server
    - Changed the interpreter path in ecosystem.config.js to use the specific Node.js version installed via NVM, ensuring compatibility and consistency in production environments.
    
    This update aims to streamline the server setup process and enhance execution reliability.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{ 
         name: "salty-server",
         script: "app.js", 
-        interpreter: '/usr/bin/node', 
+        interpreter: '/root/.nvm/versions/node/v18.20.5/bin/node',
         instances: 1,
         autorestart: true,
         env: { NODE_ENV: "production", PORT: 5010 } 

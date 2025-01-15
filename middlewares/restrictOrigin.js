dc78a51486f858de8403659452de612e7e11428d
Ryan Coughlin <ryan@ryancoughlin.com>
2025-01-15 17:28:33 -0500

    clean up cors

--- middlewares/restrictOrigin.js
+++ middlewares/restrictOrigin.js
@@ -1,33 +1,14 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl, Postman)
-    if (!origin) {
-      return callback(null, true);
-    }
-
-    const allowedOrigins = [
-      "http://localhost:5173", // Vite default
-      "http://localhost:3000", // Common React default
-      "http://localhost:5010", // Your API server
-      "https://saltyoffshore.com",
-      "https://www.saltyoffshore.com",
-      "https://conditions.saltyoffshore.com",
-    ];
-
-    if (
-      allowedOrigins.indexOf(origin) !== -1 ||
-      process.env.NODE_ENV === "development"
-    ) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: [
+    "http://localhost:5173",
+    "https://saltyoffshore.com",
+    "https://www.saltyoffshore.com",
+    "https://conditions.saltyoffshore.com",
+  ],
+  methods: ["GET", "OPTIONS"],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 module.exports = cors(corsOptions);
